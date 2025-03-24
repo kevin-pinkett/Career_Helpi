2d5256079454d29907a8e42f4fbec044df42962e
Nolan <nolanv@udel.edu>
2025-03-24 18:49:03 -0400

    fixed inital page state

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ if (prevKey !== null) {
 
 function App() {
   const [key, setKey] = useState<string>(keyData); //for api key input
-  const [page, setPage] = useState<string>('home');
+  const [page, setPage] = useState<string>('homePage');
   
   //sets the local storage item to the api key the user inputed
   function handleSubmit() {
