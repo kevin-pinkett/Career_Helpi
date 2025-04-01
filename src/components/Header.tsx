b1254874a2a32529f7319ea5b410243c4a62fbef
Nolan <nolanv@udel.edu>
2025-04-01 16:36:52 -0400

    working on disabling header buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './Header.css';
 import { Button } from "react-bootstrap";
 
@@ -9,8 +10,9 @@ interface HeaderProps {
 
 export function Header({page, setPage}: HeaderProps) {
 
-    const homeState = () => {
+      const homeState = () => {
         setPage("homePage");
+
       };
       const basicButtonState = () => {
         setPage("basicPage");
@@ -25,10 +27,14 @@ export function Header({page, setPage}: HeaderProps) {
     return (
     <div className="Header">
       <div style={{ display: 'flex', width: '100%' }}>
-    <Button style={{ flex: 1 }} className="Header-Button" onClick={homeState}>Home Page</Button>
-    <Button style={{ flex: 1 }}className="Header-Button" onClick={basicButtonState}>Basic Questions</Button>
-    <Button style={{ flex: 1 }}className="Header-Button" onClick={detailedButtonState}>Detailed Questions</Button>
-    <Button style={{ flex: 1 }}className="Header-Button" onClick={faqState}>FAQ</Button>
+    <Button style={{ flex: 1 }} className={page === "homePage" ? "Header-ButtonSelected" : "Header-ButtonUnselected"}  
+    onClick={homeState} disabled={page === "homePage"}>Home Page</Button>
+    <Button style={{ flex: 1 }}className={page === "basicPage" ? "Header-ButtonSelected" : "Header-ButtonUnselected"} 
+    onClick={basicButtonState} disabled={page === "basicPage"}>Basic Questions</Button>
+    <Button style={{ flex: 1 }}className={page === "detailedPage" ? "Header-ButtonSelected" : "Header-ButtonUnselected"} 
+    onClick={detailedButtonState} disabled={page === "detailedPage"}>Detailed Questions</Button>
+    <Button style={{ flex: 1 }}className={page === "faqPage" ? "Header-ButtonSelected" : "Header-ButtonUnselected"} 
+    onClick={faqState} disabled={page === "faqPage"}>FAQ</Button>
     </div>
     </div>
 )
