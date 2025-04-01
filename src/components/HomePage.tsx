8320ace55fda2c6a5d1e99b0bd7b0af20c90f450
Crissa Owens <crissao@udel.edu>
2025-04-01 08:53:20 -0400

    working on basic question option

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,12 +1,11 @@
-
-
-
+import React, { useState } from 'react';
+import { BasicQuestionsOption } from "./BasicQOption";
 
 export function HomePage() {
-
     return (
     <div className="HomePage">
     <h1>Home Page</h1>
+    {/* <div><BasicQuestionsOption page={"basicQuestion"} setPage={setPage} /></div> */}
     </div>
 )
 }
