9f73fbacf9b24493808b69289c720a33af50312e
Nolan <nolanv@udel.edu>
2025-04-01 16:42:36 -0400

    successfully added disabled button color

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,13 +27,13 @@ export function Header({page, setPage}: HeaderProps) {
     return (
     <div className="Header">
       <div style={{ display: 'flex', width: '100%' }}>
-    <Button style={{ flex: 1 }} className={page === "homePage" ? "Header-ButtonSelected" : "Header-ButtonUnselected"}  
+    <Button style={{ flex: 1 }} className="Header-Button"  
     onClick={homeState} disabled={page === "homePage"}>Home Page</Button>
-    <Button style={{ flex: 1 }}className={page === "basicPage" ? "Header-ButtonSelected" : "Header-ButtonUnselected"} 
+    <Button style={{ flex: 1 }}className="Header-Button" 
     onClick={basicButtonState} disabled={page === "basicPage"}>Basic Questions</Button>
-    <Button style={{ flex: 1 }}className={page === "detailedPage" ? "Header-ButtonSelected" : "Header-ButtonUnselected"} 
+    <Button style={{ flex: 1 }}className="Header-Button" 
     onClick={detailedButtonState} disabled={page === "detailedPage"}>Detailed Questions</Button>
-    <Button style={{ flex: 1 }}className={page === "faqPage" ? "Header-ButtonSelected" : "Header-ButtonUnselected"} 
+    <Button style={{ flex: 1 }}className="Header-Button" 
     onClick={faqState} disabled={page === "faqPage"}>FAQ</Button>
     </div>
     </div>
