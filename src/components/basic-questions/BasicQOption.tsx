92f32d29c266d5a4d8cdecf8a557fcec3bd8b0a5
Kevin Pinkett <kpinkett@udel.edu>
2025-04-15 22:24:09 -0400

    homepage tests

--- src/components/basic-questions/BasicQOption.tsx
+++ src/components/basic-questions/BasicQOption.tsx
@@ -5,14 +5,20 @@ interface BasicQuestionProps {
   }
 
 export function BasicQuestionsOption({setPage}: BasicQuestionProps) {
-    return (    <div style={{ display: "flex", justifyContent: "center", gap: "20px", marginTop: "20px" }}>
+    return (
+    <div
+      data-testid="basic-option"
+      style={{ 
+        display: "flex", 
+        justifyContent: "center", 
+        gap: "20px", 
+        marginTop: "20px" }}>
       <div
         style={{
           textAlign: "center",
           fontSize: "20px",
           maxWidth: "300px",
-        }}
-      >
+        }}>
         <p>Quick and easy career guidance! Answer a few straight forward questions to receive personalized insights without the deep dive.</p>
         <Button id="BasicQuestions-Button" className="Button" onClick={() => setPage("basicPage")}>
           Start Quiz
---
+++ src/components/home-page/HomePage.test.tsx
@@ -0,0 +1,55 @@
+import { fireEvent, render, screen, within } from "@testing-library/react"
+import { HomePage } from "./HomePage"
+
+describe("Home Page Component tests", () =>{
+    test("Home Page is visible.", () => {
+        render(<HomePage setPage={(page) => {}}/>);
+        const home = screen.getByTestId("home-page");
+        expect(home).toBeInTheDocument();
+    });
+
+    test("BasicQuestionsOption component is rendered.", () => {
+        render(<HomePage setPage={(page) => {}}/>);
+        const basicOption = screen.getByTestId("basic-option");
+        expect(basicOption).toBeInTheDocument();
+    });
+
+    test("DetailedQuestionsOption component is rendered.", () => {
+        render(<HomePage setPage={(page) => {}}/>);
+        const detailedOption = screen.getByTestId("detailed-option");
+        expect(detailedOption).toBeInTheDocument();
+    });
+
+    test("FAQOption component is rendered.", () => {
+        render(<HomePage setPage={(page) => {}}/>);
+        const faqOption = screen.getByTestId("faq-option");
+        expect(faqOption).toBeInTheDocument();
+    });
+
+    test("Clicking BasicQuestionsOption button calls setPage with 'basic-questions'.", () => {
+        const mockSetPage = jest.fn();
+        render(<HomePage setPage={mockSetPage} />);
+        const basicOption = screen.getByTestId("basic-option");
+        const basicOptionButton = within(basicOption).getByRole("button");
+        fireEvent.click(basicOptionButton);
+        expect(mockSetPage).toHaveBeenCalledWith("basicPage");
+    });
+
+    test("Clicking DetailedQuestionsOption button calls setPage with 'detailed-questions'.", () => {
+        const mockSetPage = jest.fn();
+        render(<HomePage setPage={mockSetPage} />);
+        const detailedOption = screen.getByTestId("detailed-option");
+        const detailedOptionButton = within(detailedOption).getByRole("button");
+        fireEvent.click(detailedOptionButton);
+        expect(mockSetPage).toHaveBeenCalledWith("detailedPage");
+    });
+
+    test("Clicking FAQOption button calls setPage with 'faq'.", async () => {
+        const mockSetPage = jest.fn();
+        render(<HomePage setPage={mockSetPage} />);
+        const faqOption = screen.getByTestId("faq-option");
+        const faqOptionButton = within(faqOption).getByRole("button");
+        fireEvent.click(faqOptionButton);
+        expect(mockSetPage).toHaveBeenCalledWith("faqPage");
+    });
+});
